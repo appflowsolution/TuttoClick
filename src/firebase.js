fe53f4d5f8c1698384fa14571155a58fd221604d
Norberto Urdaneta <appflowsolution@gmail.com>
2026-03-11 21:30:41 -0400

    Configure Firebase for Hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDVQ5vuHyZ5JxkmonvmsnbaAsqJHyRG_qo",
+  authDomain: "tuttoclick.firebaseapp.com",
+  projectId: "tuttoclick",
+  storageBucket: "tuttoclick.firebasestorage.app",
+  messagingSenderId: "809175032625",
+  appId: "1:809175032625:web:c81844f6cbf8be58a50610"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
